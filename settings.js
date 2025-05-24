375df93d5105045bb0df650e8a3d306001df2424
NicwrldKE <114879635+NicwrldKE@users.noreply.github.com>
2025-05-24 13:07:26 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,8 @@
 //-------------------[ BOT SETTINGS ]------------------// 
 
-// @project_name : KANGO-XMD 
-// @author : Hector 
-// @telegram : http://t.me/official_kango
-// @github : OfficialKango
-// @whatsapp : +233509977126
 
-//----------------------[ KANGO-XMD ]----------------------//
+
+//----------------------[ Nick-XMD ]----------------------//
 
 const fs = require('fs')
 const { color } = require('./lib/color')
@@ -15,12 +11,12 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || 'KANGO~hrEQWDiY#Nkrygdu320RcRaGaGIEI7zbxZctjcWCCsDGNE5hlq_w' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~x7V0FATa#u1QIBhJSyjWrLjRO3kiXaswKVrDjqAFNkB3VdbNZ6-E' 
 //Enter your KANGO-XMD session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
 
-global.botname = process.env.BOT_NAME || 'KANGO-XMD' 
+global.botname = process.env.BOT_NAME || 'Nick-XMD' 
 
 //-----------------[ OWNER NUMBER ]------------------//
 
@@ -34,15 +30,15 @@ global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['233509977126', 
 
 //-----------------[ OWNER NAME ]------------------//
 
-global.ownername = process.env.OWNER_NAME || 'Hector Manuel' 
+global.ownername = process.env.OWNER_NAME || 'Nick' 
 
 //------------[ STICKER PACKNAME ]-----------------//
 
-global.packname = process.env.STICKER_PACK_NAME || "KANGO-XMD" 
+global.packname = process.env.STICKER_PACK_NAME || "Nick-MD" 
 
 //--------------[ STICKER AUTHOR NAME ]------------//
 
-global.author = process.env.STICKER_AUTHOR_NAME || "Hector" 
+global.author = process.env.STICKER_AUTHOR_NAME || "Nick" 
 
 //----------------[ GITHUB DATABASE ]-----------------//
 
@@ -51,17 +47,17 @@ global.dbToken = process.env.GITHUB_TOKEN || "";
 
 //-----------------[ CONTEXT LINK ]--------------------//
 
-global.plink = process.env.PLINK || "https://youtube.com/@official_manuel"
+global.plink = process.env.PLINK || "https://youtube.com/"
 
 //------------------[ WATERMARK ]--------------------//
 
-global.wm = process.env.GL_WM || "> ©KANGO-XMD"
+global.wm = process.env.GL_WM || "> ©Nick-MD"
 
 //---------------------[ REPLIES ]-----------------------//
 
 global.mess = { 
   done: '*Done*', 
-  success: '©kango-xmd', 
+  success: '@Nick-md', 
   owner: `*You don't have permission to use this command!*`, 
   group: '*This feature becomes available when you use it in a group!*', 
   admin: '*You’ll unlock this feature with me as an admin!*', 
